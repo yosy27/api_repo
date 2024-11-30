38dfcf0e31dc5bf1a4e84c9e2f66d32ac98280e4
ArturoRoncal2704 <roncalhermenegildo@gmail.com>
2024-11-30 04:42:39 -0500

    Modificaciones

--- src/main/java/com/cibertec/proypid/controller/UsuarioController.java
+++ src/main/java/com/cibertec/proypid/controller/UsuarioController.java
@@ -41,7 +41,7 @@ public class UsuarioController {
         return ResponseEntity.ok(usuario);
     }
 
-    @PostMapping
+    @PostMapping ("/usuarios")
     public ResponseEntity<UsuarioEntity> registrarCliente(@RequestBody UsuarioEntity nuevoUsuario) {
         UsuarioEntity usuarioGuardado = usuarioService.registrarCliente(nuevoUsuario);
         return ResponseEntity.status(HttpStatus.CREATED).body(usuarioGuardado);
