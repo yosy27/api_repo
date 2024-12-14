112b4b82a0ccbe630f4a76de27b8fd13f5c75748
yosy27 <yoselynespinoza27@gmail.com>
2024-12-14 00:41:10 -0500

    Configuracion 2 para registrar proyecto

--- src/main/java/com/cibertec/proypid/service/impl/ProyectoServiceImpl.java
+++ src/main/java/com/cibertec/proypid/service/impl/ProyectoServiceImpl.java
@@ -1,7 +1,9 @@
 package com.cibertec.proypid.service.impl;
 
 import com.cibertec.proypid.model.entity.ProyectoEntity;
+import com.cibertec.proypid.model.entity.UsuarioEntity;
 import com.cibertec.proypid.repository.ProyectoRepository;
+import com.cibertec.proypid.repository.UsuarioRepository;
 import com.cibertec.proypid.service.ProyectoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +16,20 @@ public class ProyectoServiceImpl implements ProyectoService {
     @Autowired
     private ProyectoRepository proyectoRepository;
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @Override
     public ProyectoEntity registrarProyecto(ProyectoEntity proyecto) {
+        UsuarioEntity usuario = usuarioRepository.findById(proyecto.getUsuario().getIdUsuario()).orElse(null);
+
+        if (usuario == null) {
+            throw new RuntimeException("Usuario no encontrado");
+        }
+
+        // Asociar el usuario al proyecto
+        proyecto.setUsuario(usuario);
+
         return proyectoRepository.save(proyecto);
     }
 
